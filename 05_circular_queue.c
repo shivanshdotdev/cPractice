794874d48889abfaa2f6fea2aefb188b13a0c153
Shivansh Shukla <cashivanshshukla.coding+github@gmail.com>
2026-09-13 14:09:13 +0530

    working on circular queue, alpha implementation is done, but work is needed

--- 05_circular_queue.c
+++ 05_circular_queue.c
@@ -2,30 +2,70 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-#define MAX_SIZE 10
+#define MAX_SIZE 100
 
 typedef struct {
-    int front;
-    int rear;
+    int entry;
+    int exit;
     int data[MAX_SIZE];
 } Queue;
 
 void initialize(Queue *queue){
-    queue -> front = 0;
-    queue -> rear = 0;
+    queue -> entry = -1;
+    queue -> exit = -1;
 }
 
 bool isEmpty(Queue *queue);
+
 bool isFull(Queue *queue);
+
 void enqueue(Queue *queue, int value){
-    queue -> data[queue -> rear] = value;
-    if 
-    queue -> rear++;
+    queue -> entry++;
+    int slot = queue -> entry % MAX_SIZE;
+
+    if (slot == queue -> exit){
+        printf("The list is full\n");
+        return;
+    }
+
+    queue -> data[slot] = value;
+    queue -> entry = slot;
+    
 }
+
 int dequeue(Queue *queue){
-    int return_value = queue -> data[queue -> front];
+    queue -> exit++;
+    int return_value = queue -> data[queue -> exit];
+
+    printf("Dequeued %d\n", return_value);
+
+    return return_value;
+}
+
+void printQueue(Queue *queue){
+    int limit = queue -> entry;
+    for (int i = queue -> exit; i <= limit; i++){
+        printf("%d\t", queue -> data[i]);
+    }
+    printf("\n");
 }
 
 int main(){
 
+    Queue *queue = malloc(sizeof(Queue));
+
+    initialize(queue);
+
+    enqueue(queue, 10);
+    enqueue(queue, 20);
+    enqueue(queue, 30);
+    enqueue(queue, 40);
+
+    printQueue(queue);
+    
+    dequeue(queue);
+    dequeue(queue);
+
+
+    printQueue(queue);
 }
